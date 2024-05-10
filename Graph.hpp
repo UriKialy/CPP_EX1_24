65295f9ddda00f2305b062f628cd74a324eed262
Uri Kialy <141826647+UriKialy@users.noreply.github.com>
2024-05-10 10:54:31 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -14,13 +14,11 @@ namespace ariel
         bool isdirected;
     public:
         Graph();
-        ~Graph() {  }
-
         //this function will load the graph to the object
         void loadGraph(vector<vector<int>> g);
 
         //this function will print the graph
-        void printGraph();
+        void printGraph();  
 
         //this function will return the adjacency matrix of the graph
         std::vector<std::vector<int>> getAdjacencyMatrix();
